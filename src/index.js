b6b6dee0154abd316d792f643168da45304601a0
Artem Priadko <artem.priadko@syntech.software>
2020-09-24 16:55:05 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './redux/rootReducer';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(
+  rootReducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+import { CREATE_POST } from "./types";
+
+import {CREATE_POST} from './types'
+
+export function createPost() {
+    return {
+        type: CREATE_POST,
+        payload
+    }
+}
